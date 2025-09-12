9a633cf5c2d494d7719ca81836f64cca49e29f97
Paulo Kuster <paulorhyank@gmail.com>
2025-09-11 22:32:49 -0300

    fix: user model bug

--- app/models/user_model.py
+++ app/models/user_model.py
@@ -2,6 +2,7 @@ from app import db
 from sqlalchemy.dialects.postgresql import UUID
 import uuid
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model):
     __tablename__ = 'user' # Define o nome da tabela
@@ -17,3 +18,14 @@ class User(db.Model):
     dt_user_last_login = db.Column(db.DateTime, nullable=True) # Nullable, pois o user ainda não logou ao ser criado
 
     transactions = db.relationship('Transaction', backref='user', lazy=True)
+    
+    def set_password(self, password):
+        """Cria o hash da senha e armazena no campo password_hash"""
+        self.password_hash = generate_password_hash(password)
+    
+    def check_password(self, password):
+        """Verifica se a senha fornecida corresponde ao hash armazenado"""
+        return check_password_hash(self.password_hash, password)
+    
+    def __repr__(self):
+        return f'<User {self.ds_user_email}>'
